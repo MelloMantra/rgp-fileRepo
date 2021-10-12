849f59d898e5ebc16ff69ea615879f8851d02f60
MelloMantra <67211433+MelloMantra@users.noreply.github.com>
2021-10-11 19:16:30 -0700

    Add files via upload

---
+++ nestopiaUnzipper.bat
@@ -0,0 +1,19 @@
+@echo off
+setlocal
+cd /d %~dp0
+Call :UnZipFile "OneDrive - Chandler Unified School District #80\RetroGamePackage\Starter_Pack\Contents\emuStore\" "OneDrive - Chandler Unified School District #80\RetroGamePackage\Starter_Pack\Contents\emuStore\Nestopia.zip"
+exit /b
+:UnZipFile <ExtractTo> <newzipfile>
+set vbs="%temp%\_.vbs"
+if exist %vbs% del /f /q %vbs%
+>>%vbs% echo Set fso = CreateObject("Scripting.FileSystemObject")
+>>%vbs% echo If NOT fso.FolderExists(%1) Then
+>>%vbs% echo fso.CreateFolder(%1)
+>>%vbs% echo End If
+>>%vbs% echo set objShell = CreateObject("Shell.Application")
+>>%vbs% echo set FilesInZip=objShell.NameSpace(%2).items
+>>%vbs% echo objShell.NameSpace(%1).CopyHere(FilesInZip)
+>>%vbs% echo Set fso = Nothing
+>>%vbs% echo Set objShell = Nothing
+cscript //nologo %vbs%
+if exist %vbs% del /f /q %vbs%
